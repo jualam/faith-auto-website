073d4510f7a45ebca2d3c1cbec29b29f2208d9a8
faiaz112 <jualam@ttu.edu>
2025-02-18 22:48:30 -0600

    new commit

---
+++ faith-auto-fe/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div><h1 className='text-7xl text-sky-300'>HI thisssssssssss is faith automobile</h1></div>
+  )
+}
+
+export default App
